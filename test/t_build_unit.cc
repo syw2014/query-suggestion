168b4997b8fc77cca2599fd3855cd06598c8eed4
Jerry.Shi <jerryshi0110@gmail.com>
2016-07-22 15:42:46 +0800

    start to design unit tests for data building module

---
+++ test/t_build_unit.cc
@@ -0,0 +1,18 @@
+/*************************************************************************
+ @ File Name: t_build_unit.cc
+ @ Method: 
+ @ Author: Jerry Shi
+ @ Mail: jerryshi0110@gmail.com
+ @ Created Time: Thu 21 Jul 2016 04:21:35 PM CST
+ ************************************************************************/
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE DataBuildEngine
+
+#include <iostream>
+#include "unit_test.h"
+
+BOOST_AUTO_TEST_SUITE (BuildEngine)
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
